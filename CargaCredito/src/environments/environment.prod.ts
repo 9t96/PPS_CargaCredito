10fe31d67518b53af8ea2468e3ee695d28f55966
laloo96 <ohmycaio@gmail.com>
2020-04-12 16:32:18 -0300

    -Agrego login funcionando
    -Routing
    -Auth

---
+++ CargaCredito/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCtKzY6RfIkNwwfsdpSuB4P4IPdbxb7RIA",
+    authDomain: "cargar-credito.firebaseapp.com",
+    databaseURL: "https://cargar-credito.firebaseio.com",
+    projectId: "cargar-credito",
+    storageBucket: "cargar-credito.appspot.com",
+    messagingSenderId: "871805228504",
+    appId: "1:871805228504:web:d556201a2026fd7a518ead"
+  }
+};
---
+++ CargaCredito/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCtKzY6RfIkNwwfsdpSuB4P4IPdbxb7RIA",
+    authDomain: "cargar-credito.firebaseapp.com",
+    databaseURL: "https://cargar-credito.firebaseio.com",
+    projectId: "cargar-credito",
+    storageBucket: "cargar-credito.appspot.com",
+    messagingSenderId: "871805228504",
+    appId: "1:871805228504:web:d556201a2026fd7a518ead"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
